48dd9415ab0cc48169fd2a817cefa459de435a06
deepa-varghese-88 <dv8@pdx.edu>
2020-03-02 00:25:38 -0800

    hackaround for login

--- app.js
+++ app.js
@@ -1,37 +1,74 @@
+require("dotenv").config(); //for .env file with config details
 const express = require("express");
 const path = require("path");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
 
 // initialising the app
 const app = express();
 
 //Defining the port
 const PORT = process.env.PORT || 5000;
+//url for mongodb connection
+const url = process.env.MONGODB_URI;
 
+//Middleware to run when any call is made
+//static for accessing html/css/image files inside public folder
 app.use(express.static(path.join(__dirname, "/public")));
+//body-parser to read user input
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//HOME page--------------------------------------------------------------------------------
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/HomePage.html"));
 });
 
+//LOGIN page--------------------------------------------------------------------------------
+//get
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public/login.html"));
 });
 
+//post
 app.post("/submit", (req, res) => {
-  //check if user exists in mongodb
-  //check if user is admin or user
-  //assign necessary variables accordingly
+  MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
+    if (err) throw err;
+    client
+      .db("cfairdb")
+      .collection("users")
+      .findOne({ email: req.body.email }, (err, result) => {
+        if (result == null || result.password != req.body.password) {
+          res.redirect(path.join(__dirname, "/public/login.html"));
+        } else {
+          if (result.usertype == "user") {
+            res.sendFile(path.join(__dirname, "/public/cfairmap.html"));
+          } else {
+            res.sendFile(path.join(__dirname, "/public/HomePage.html"));
+          }
+        }
+      });
+  });
   //res.redirect('/users/' + req.user.username);
 });
 
-// //resolve request for admin page
+//ADMIN page--------------------------------------------------------------------------------
+//get
 // app.get("/Admin", (req, res) => {
 
 // });
 
-//resolve request for cfair map
+//CFAIR MAP page-----------------------------------------------------------------------------
 app.get("/cfairmap", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/cfairmap.html"));
+  // MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
+  //   if (err) throw err;
+  //   client
+  //     .db("cfairdb")
+  //     .collection("users")
+  //     .find()
+  //     .each((err, doc) => {
+  //     });
+  //connect to Database.companies and get the list of companies
 });
 
 app.listen(PORT, () => {
