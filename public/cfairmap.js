fd0129fd1e356772b81c6bcf3184d356f5d8cb15
deepa-varghese-88 <dv8@pdx.edu>
2020-03-05 02:07:34 -0800

    Single Click and Double Click functions ready - yellow, green and company details

--- public/cfairmap.js
+++ public/cfairmap.js
@@ -1,53 +1,34 @@
 // Fit variable sized company names inside fixed sized div blocks
 textFit(document.getElementsByClassName("comp-block"));
 
-// function trial(compid) {
-//   // Get the modal
-//   var modal = document.getElementById("myModal");
-
-//   // Get the button that opens the modal
-//   var btn = document.getElementById("myBtn");
-
-//   // Get the <span> element that closes the modal
-//   var span = document.getElementsByClassName("close")[0];
-
-//   // When the user clicks the button, open the modal
-//   btn.onclick = function() {
-//     modal.style.display = "block";
-//   };
-
-//   // When the user clicks on <span> (x), close the modal
-//   span.onclick = function() {
-//     modal.style.display = "none";
-//   };
-
-//   // When the user clicks anywhere outside of the modal, close it
-//   window.onclick = function(event) {
-//     if (event.target == modal) {
-//       modal.style.display = "none";
-//     }
-//   };
-// }
-
+// Set whiteColor to identify background of company block and set it to yellow on click
 var whiteColor = $("<div/>")
   .css({
     "background-color": "#ffffff"
   })
   .css("background-color");
+
+// Set yellowColor to check if background of company block is yellow and set it to green on click
 var yellowColor = $("<div/>")
   .css({
     "background-color": "#f9fd0a"
   })
   .css("background-color");
 
-var DELAY = 300,
-  clicks = 0,
-  timer = null;
+//set Delay and timer for function that differentiates between single click and double click
+var DELAY = 300;
+var timer = null;
 
-function compactions(el, compid) {
+//Function output: Actions to be performed on a single or double click
+function compactions(el, num) {
+  var compid = "comp-id-" + num;
+  var modalid = "modal-id-" + num;
+  var modal = document.getElementById(modalid);
+  var span = document.getElementsByClassName("close")[0];
   if (el.getAttribute("data-dblclick") == null) {
     el.setAttribute("data-dblclick", 1);
     timer = setTimeout(function() {
+      //Action for single click
       if (el.getAttribute("data-dblclick") == 1) {
         if ($("#" + compid).css("background-color") == whiteColor) {
           $("#" + compid).css("background-color", "#f9fd0a");
@@ -60,7 +41,16 @@ function compactions(el, compid) {
       el.removeAttribute("data-dblclick");
     }, DELAY);
   } else {
+    //Action for double click
     el.removeAttribute("data-dblclick");
-    alert("doubleclick");
+    modal.style.display = "block";
+    span.onclick = function() {
+      modal.style.display = "none";
+    };
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
   }
 }
