7da26a2172232f3ad7bd37d83555d4436628ad39
deepa-varghese-88 <dv8@pdx.edu>
2020-03-04 23:48:41 -0800

    div single click and double click working

--- public/cfairmap.js
+++ public/cfairmap.js
@@ -14,18 +14,73 @@ textFit(document.getElementsByClassName("comp-block"));
 //   }
 // }
 
-var whiteColor = $(".comp-block").css("background-color");
+// function compdesc(company) {
+//   alert(company);
+// }
+
+// function trial(compid) {
+//   // Get the modal
+//   var modal = document.getElementById("myModal");
+
+//   // Get the button that opens the modal
+//   var btn = document.getElementById("myBtn");
+
+//   // Get the <span> element that closes the modal
+//   var span = document.getElementsByClassName("close")[0];
+
+//   // When the user clicks the button, open the modal
+//   btn.onclick = function() {
+//     modal.style.display = "block";
+//   };
+
+//   // When the user clicks on <span> (x), close the modal
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   };
+
+//   // When the user clicks anywhere outside of the modal, close it
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   };
+// }
+
+var whiteColor = $("<div/>")
+  .css({
+    "background-color": "#ffffff"
+  })
+  .css("background-color");
 var yellowColor = $("<div/>")
   .css({
     "background-color": "#f9fd0a"
   })
   .css("background-color");
-$(".comp-block").click(function() {
-  if ($(this).css("background-color") == whiteColor) {
-    $(this).css("background-color", "#f9fd0a");
-  } else if ($(this).css("background-color") == yellowColor) {
-    $(this).css("background-color", "#c0e908");
-  } else {
-    $(this).css("background-color", whiteColor);
-  }
-});
+
+var DELAY = 400,
+  clicks = 0,
+  timer = null;
+
+$(".comp-block")
+  .on("click", function() {
+    clicks++;
+    if (clicks === 1) {
+      timer = setTimeout(function() {
+        if ($(".comp-block").css("background-color") == whiteColor) {
+          $(".comp-block").css("background-color", "#f9fd0a");
+        } else if ($(".comp-block").css("background-color") == yellowColor) {
+          $(".comp-block").css("background-color", "#c0e908");
+        } else {
+          $(".comp-block").css("background-color", whiteColor);
+        }
+        clicks = 0; //after action performed, reset counter
+      }, DELAY);
+    } else {
+      clearTimeout(timer); //prevent single-click action
+      alert("Double Click"); //perform double-click action
+      clicks = 0; //after action performed, reset counter
+    }
+  })
+  .on("dblclick", function(e) {
+    e.preventDefault(); //cancel system double-click event
+  });
