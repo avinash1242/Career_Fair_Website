6482cc1c5f0ee0cf15d6e542841eef43ffe40066
AvinashVCherukuri <avin2@pdx.edu>
2020-03-13 12:29:33 -0700

    Upload

--- app.js
+++ app.js
@@ -56,6 +56,29 @@ app.post("/submit", (req, res) => {
   });
 });
 
+//Uploading File and Submitting
+//var express = require("express");
+var multer  = require('multer');
+//var app = express();
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage : storage}).single('userPhoto');
+
+app.post('/api/photo',function(req,res){
+  upload(req,res,function(err) {
+    if(err) {
+      return res.end("Error uploading file.");
+    }
+    res.end("File is uploaded");
+  });
+});
+
 //ADMIN page--------------------------------------------------------------------------------
 app.get("/AdminPage", (req, res) => {
   //connect to Database.companies and get the list of companies
